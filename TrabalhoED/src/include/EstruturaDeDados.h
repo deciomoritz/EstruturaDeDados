37c5cb35b01a9665ad69d2a9e23e7ae7d954deed
Decio <deciomoritz1@gmail.com>
2013-08-29 14:14:26 -0300

    Inseridos comentários doxygen

--- TrabalhoED/src/include/EstruturaDeDados.h
+++ TrabalhoED/src/include/EstruturaDeDados.h
@@ -1,40 +1,40 @@
-template <typename Tipo>
-class EstruturaDeDados{
+template<typename Tipo>
+class EstruturaDeDados {
 
-	public:
+public:
 
-/** @brief Adiciona elemento na estrutura de dados
-*
-* @param t é o elemento que será adicionado à estrutura
-* 
-* @return void
-*
-* @remarks Essa função não pode ser chamada com a estrutura cheia
-*/
-		virtual void adicionar(Tipo t) = 0;
-/** @brief Remove elemento da estrutura de dados
-* 
-* @return Elemento retirado da estrutura
-*
-* @remarks Essa função não pode ser chamada com a estrutura vazia
-*/
-		virtual Tipo remover() = 0;
-/** @brief Informa tamanho da lista
-* 
-* @return Inteiro representando o tamanho da lista
-*
-*/
-		virtual int tamanho() = 0;
-/** @brief Informa se lista está cheia
-* 
-* @return true se lista estiver cheia
-*
-*/
-		virtual bool cheia() = 0;
-/** @brief Informa se lista está vazia
-* 
-* @return true se lista estiver vazia
-*
-*/
-		virtual bool vazia() = 0;
+	/** @brief Adiciona elemento na estrutura de dados
+	 *
+	 * @param t é o elemento que será adicionado à estrutura
+	 *
+	 * @return void
+	 *
+	 * @remarks Essa função não pode ser chamada com a estrutura cheia
+	 */
+	virtual void adicionar(Tipo t) = 0;
+	/** @brief Remove elemento da estrutura de dados
+	 *
+	 * @return Elemento retirado da estrutura
+	 *
+	 * @remarks Essa função não pode ser chamada com a estrutura vazia
+	 */
+	virtual Tipo remover() = 0;
+	/** @brief Informa tamanho da lista
+	 *
+	 * @return Inteiro representando o tamanho da lista
+	 *
+	 */
+	virtual int tamanho() = 0;
+	/** @brief Informa se lista está cheia
+	 *
+	 * @return true se lista estiver cheia
+	 *
+	 */
+	virtual bool cheia() = 0;
+	/** @brief Informa se lista está vazia
+	 *
+	 * @return true se lista estiver vazia
+	 *
+	 */
+	virtual bool vazia() = 0;
 };
--- TrabalhoED/src/include/RecebeDados.h
+++ TrabalhoED/src/include/RecebeDados.h
@@ -2,17 +2,17 @@
 
 class RecebeDados {
 public:
-/** @brief Recebe um inteiro a partir de entrada de teclado
-* 
-* @return int
-*
-*/
+	/** @brief Recebe um inteiro a partir de entrada de teclado
+	 *
+	 * @return int
+	 *
+	 */
 	int pegaInt();
-/** @brief Recebe um string a partir de entrada de teclado
-* 
-* @return string
-*
-*/
+	/** @brief Recebe um string a partir de entrada de teclado
+	 *
+	 * @return string
+	 *
+	 */
 	std::string pegaString();
 
 };
